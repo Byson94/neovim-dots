987398b5cda3dcf72d24524c6eeeb2b9c7c77651
Byson94 <byson94@outlook.com>
2026-06-07 12:25:05 +0530

    get rid of treesitter

--- init.lua
+++ init.lua
@@ -1,1 +1,8 @@
 require("config.lazy")
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'svelte', 'rust', 'html', 'css', 'javascript', 'python', 'astro', 'cpp', 'c' },
+  callback = function()
+    vim.treesitter.start()
+  end,
+})
