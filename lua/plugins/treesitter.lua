ea9c31ea64edbc4493bf48a82ea795b235e27b95
Byson94 <byson94@outlook.com>
2026-06-07 13:37:37 +0530

    many changes and stuff

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,11 @@ return {
   lazy = false,
   build = ':TSUpdate',
   config = function()
-    require('nvim-treesitter').install({ 'rust', 'html', 'css', 'javascript', 'cpp', 'c', 'python', 'astro', 'svelte' })
+    require('nvim-treesitter').install({ 
+        'rust', 'html', 'html_tags', 'css', 'javascript', 'typescript', 'cpp', 'c', 'python', 'astro', 'svelte' 
+    })
+    require('nvim-treesitter').setup {
+        install_dir = vim.fn.stdpath('data') .. '/site'
+    }
   end
 }
