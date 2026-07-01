de4dee8fec9e09a09891d2032d3b53c7774c276d
Byson94 <byson94@outlook.com>
2026-07-01 19:32:01 +0530

    feat: remove nvim-treesitter

--- init.lua
+++ init.lua
@@ -1,1 +1,11 @@
 require("config.lazy")
+
+vim.api.nvim_create_autocmd('FileType', {
+  desc = 'Enable native Tree-sitter highlighting and folding',
+  callback = function()
+    local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
+    if lang then
+      pcall(vim.treesitter.start)
+    end
+  end,
+})
